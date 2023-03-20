1c1567c620cf55fce8bb97a0877b4ba3017f8720
Anastasia <102552745+avetkovskaya@users.noreply.github.com>
2023-03-20 23:04:55 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 const { default: mongoose } = require('mongoose');
 const { NOT_FOUND, CAST_ERROR } = require('./users');
 
 const Cards = require('../models/cards');
-=======
-const { default: mongoose } = require("mongoose");
-const Cards = require("../models/cards");
-const { NOT_FOUND, CAST_ERROR } = require("../base");
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
 
 module.exports.getCard = (req, res) => {
   Cards.find({})
@@ -27,11 +21,7 @@ module.exports.createCard = (req, res) => {
         return res
           .status(400)
           .send({
-<<<<<<< HEAD
             message: 'Переданы некорректные данные при создании карточки',
-=======
-            message: "Переданы некорректные данные при создании карточки",
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
           });
       }
       return res.status(500).send({ message: err.message });
@@ -48,20 +38,12 @@ module.exports.deleteCard = (req, res) => {
       if (err.name === CAST_ERROR) {
         return res
           .status(400)
-<<<<<<< HEAD
           .send({ message: 'Переданы некорректные данные карточки' });
-=======
-          .send({ message: "Переданы некорректные данные карточки" });
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
       }
       if (err.message === NOT_FOUND) {
         return res
           .status(404)
-<<<<<<< HEAD
           .send({ message: 'Карточка с указанным _id не найдена' });
-=======
-          .send({ message: "Карточка с указанным _id не найдена" });
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
       }
       return res.status(500).send({ message: err.message });
     });
@@ -71,11 +53,7 @@ module.exports.likeCard = (req, res) => {
   Cards.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
-<<<<<<< HEAD
     { new: true },
-=======
-    { new: true }
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
   )
     .orFail(new Error(NOT_FOUND))
     .then((card) => {
@@ -86,21 +64,13 @@ module.exports.likeCard = (req, res) => {
         return res
           .status(400)
           .send({
-<<<<<<< HEAD
             message: 'Переданы некорректные данные для постановки лайка',
-=======
-            message: "Переданы некорректные данные для постановки лайка",
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
           });
       }
       if (err.message === NOT_FOUND) {
         return res
           .status(404)
-<<<<<<< HEAD
           .send({ message: 'Передан несуществующий _id карточки' });
-=======
-          .send({ message: "Передан несуществующий _id карточки" });
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
       }
       return res.status(500).send({ message: err.message });
     });
@@ -110,11 +80,7 @@ module.exports.dislikeCard = (req, res) => {
   Cards.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
-<<<<<<< HEAD
     { new: true },
-=======
-    { new: true }
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
   )
     .orFail(new Error(NOT_FOUND))
     .then((card) => {
@@ -124,20 +90,12 @@ module.exports.dislikeCard = (req, res) => {
       if (err.name === CAST_ERROR) {
         return res
           .status(400)
-<<<<<<< HEAD
           .send({ message: 'Переданы некорректные данные для снятии лайка' });
-=======
-          .send({ message: "Переданы некорректные данные для снятии лайка" });
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
       }
       if (err.message === NOT_FOUND) {
         return res
           .status(404)
-<<<<<<< HEAD
           .send({ message: 'Передан несуществующий _id карточки' });
-=======
-          .send({ message: "Передан несуществующий _id карточки" });
->>>>>>> 825f75d9f8fd908763c0881e8d8a63ef7989e24b
       }
       return res.status(500).send({ message: err.message });
     });
