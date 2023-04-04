ff127a25ef31130bb6f501e0f95693f8a4f463bd
Anastasia <102552745+avetkovskaya@users.noreply.github.com>
2023-04-04 23:23:47 +0300

    PR-14 after review

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const NotFoundError = require('../errors/NotFoundError');
+const { login, createUser } = require('../controllers/users');
+const { validAuthName } = require('../middlewares/joiValidation');
+const auth = require('../middlewares/auth');
+const allowedCors = require('../middlewares/allowedCors');
+
+router.use(allowedCors);
+
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+router.post('/signin', validAuthName, login);
+router.post('/signup', validAuthName, createUser);
+router.use(auth);
+router.use('/users', require('./users'));
+router.use('/cards', require('./cards'));
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Маршрут не найден.'));
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,24 @@
 const router = require('express').Router();
 const {
+  validId,
+  validAuthName,
+  validUserInfo,
+  userAvatarValid,
+} = require('../middlewares/joiValidation');
+const {
   getUsers,
   getUser,
   createUser,
   updateUser,
   updateAvatar,
+  logout,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/me', getUser);
-router.post('/', createUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateAvatar);
-
+router.delete('/signout', logout);
+router.get('/:userId', validId('userId'), getUser);
+router.post('/', validAuthName, createUser);
+router.patch('/me', validUserInfo, updateUser);
+router.patch('/me/avatar', userAvatarValid, updateAvatar);
 module.exports = router;
